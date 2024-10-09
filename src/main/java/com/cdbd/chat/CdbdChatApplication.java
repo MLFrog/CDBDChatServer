899fa230e66e78dcc8689a7e08c535c92365e74d
CASY82 <repaid11010@gmail.com>
2024-10-10 01:05:37 +0900

    init project

---
+++ src/main/java/com/cdbd/chat/CdbdChatApplication.java
@@ -0,0 +1,13 @@
+package com.cdbd.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbdChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdbdChatApplication.class, args);
+	}
+
+}
